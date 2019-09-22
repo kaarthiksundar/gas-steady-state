831162244033e74924b722fd3c1566d882d58d4b
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2019-09-22 11:01:07 +0400

    components header file

---
+++ include/components.h
@@ -0,0 +1,163 @@
+#ifndef COMPONENTS_H
+#define COMPONENTS_H
+
+#pragma once
+
+#include <string>
+
+class Pipe {
+private:
+    bool _empty = true;
+    int _id;
+    std::string _name;
+    int _fnode;
+    int _tnode;
+    double _diameter;
+    double _length;
+    double _friction_factor;
+    int _num_discretization_segments;
+    bool _standard_units;
+    bool _si_units;
+    bool _status;
+    bool _per_unit;
+    
+public:
+    Pipe();
+    Pipe(int id, std::string name, int fnode, int tnode,
+         double diameter, double length, double friction_factor,
+         int num_discretization_segments, int units);
+    
+    bool is_empty() const;
+    std::string get_name() const;
+    int get_id() const;
+    int get_fnode_id() const;
+    int get_tnode_id() const;
+    double get_diameter() const;
+    double get_length() const;
+    double get_friction_factor() const;
+    int get_num_discretization_segments() const;
+    bool is_standard() const;
+    bool is_si() const;
+    bool is_per_unit() const;
+    bool get_status() const;
+    
+    void set_filled();
+    void make_per_unit();
+    void make_standard();
+    void make_si();
+    
+    friend std::ostream& operator<<(std::ostream& os, const Pipe &pipe);
+};
+
+class Node {
+private:
+    bool _empty = true;
+    int _id;
+    std::string _name;
+    std::pair<double, double> _coords;
+    double _pmin;
+    double _pmax;
+    double _injection_min;
+    double _injection_max;
+    bool _slack;
+    bool _standard_units;
+    bool _si_units;
+    bool _status;
+    bool _per_unit;
+    
+public:
+    Node();
+    Node(int id, std::string name, double x, double y,
+         double pmin, double pmax, double injection_min,
+         double injection_max, bool slack, int units);
+    
+    bool is_empty() const;
+    int get_id() const;
+    std::string get_name() const;
+    std::pair<double, double> get_coords() const;
+    double get_pmin() const;
+    double get_pmax() const;
+    double get_injection_min() const;
+    double get_injection_max() const;
+    bool is_slack() const;
+    bool is_si() const;
+    bool is_standard() const;
+    bool is_per_unit() const;
+    
+    void set_filled();
+    void make_per_unit();
+    void make_standard();
+    void make_si();
+    friend std::ostream& operator<<(std::ostream& os, const Junction &node);
+};
+
+class Compressor {
+private:
+    bool _empty = true;
+    int _id;
+    std::string _name;
+    int _fnode;
+    int _tnode;
+    double _cmin;
+    double _cmax;
+    double _power_max;
+    double _flow_min;
+    double _flow_max;
+    double _diameter;
+    double _length;
+    double _friction_factor;
+    bool _standard_units;
+    bool _si_units;
+    bool _status;
+    bool _per_unit;
+    
+public:
+    Compressor();
+    Compressor(int id, std::string name, int fnode, int tnode,
+               double cmin, double cmax, double power_max,
+               double flow_min, double flow_max, int units);
+    
+    bool is_empty() const;
+    int get_id() const;
+    std::string get_name() const;
+    int get_fnode_id() const;
+    int get_tnode_id() const;
+    double get_cmin() const;
+    double get_cmax() const;
+    double get_power_max() const;
+    double get_diameter() const;
+    double get_length() const;
+    double get_friction_factor() const;
+    bool is_standard() const;
+    bool is_si() const;
+    bool is_per_unit() const;
+    
+    void set_filled();
+    void make_per_unit();
+    void make_standard();
+    void make_si();
+    friend std::ostream& operator<<(std::ostream& os, const Compressor &compressor);
+};
+
+class Gnode {
+private:
+    bool _empty = true;
+    int _id;
+    std::string _name;
+    int _node;
+    bool _status;
+    
+public:
+    Gnode();
+    Gnode(int id, std::string name, int node);
+    
+    bool is_empty() const;
+    int get_id() const;
+    std::string get_name() const;
+    int get_node_id() const;
+    
+    void set_filled();
+    friend std::ostream& operator<<(std::ostream& os, const Gnode &gnode);
+};
+
+#endif
